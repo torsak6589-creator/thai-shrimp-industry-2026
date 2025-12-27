7d14e84ccd88b8d59b0f9f46929c8fa71c01d8fb
torsak6589-creator <torsak6589@gmail.com>
2025-12-27 14:55:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
