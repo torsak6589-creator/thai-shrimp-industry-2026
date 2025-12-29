2f6481471bbdc6176a69b032ca961111471dd410
torsak6589-creator <torsak6589@gmail.com>
2025-12-29 08:14:35 +0700

    Fetch and display prices from Google Sheets
    Added a function to fetch prices from Google Sheets and updated the dashboard route to display these prices.

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
+import requests, json
+
+def get_price_from_gsheet():
+    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT6x4gRN0oCi3gUzWQsAFl4BdefiaRg2EsSahflCpfCT7l4Ahokeq5vV2PCEtMJvrilljmVdVQOMLZO/gviz/tq?tqx=out:json&gid=1530942221"
+
+    r = requests.get(url, timeout=15)
+    text = r.text
+    json_data = text[text.find("{"):text.rfind("}")+1]
+    data = json.loads(json_data)
+
+    rows = data["table"]["rows"]
+    prices = []
+
+    for row in rows[-7:]:
+        c = row["c"]
+        prices.append({
+            "date": c[0]["v"],
+            "size": c[1]["v"],
+            "price": c[2]["v"]
+        })
+
+    return prices
 
 @app.route("/")
 def home():
@@ -8,4 +30,6 @@ def home():
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    prices = get_price_from_gsheet()
+    return render_template("dashboard.html", prices=prices)
+
